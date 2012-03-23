668edcbaa0c13dda2b5c7e35da26180958d1a133
Scott Penrose <scottp@dd.com.au>
2012-03-24 08:35:21 +1100

    Attempt basic file version

--- PerlBone.pm
+++ PerlBone.pm
@@ -20,9 +20,18 @@ sub run {
 sub digitalWrite {
 	my ($pin, $val) = @_;
 	print STDERR "Writing $pin = $val\n";
+	open (my $out, ">", "/sys/class/gpio/gpio$pin/value");
+	print $out $val eq 'HIGH' ? 1 : 0;
+	close $out;
 }
 
 sub digitalRead {
+	my ($pin, $val) = @_;
+	print STDERR "Writing $pin = $val\n";
+	open (my $out, "<", "/sys/class/gpio/gpio$pin/value");
+	my $in = <$out>;
+	chomp($in);
+	return $in == '1';
 }
 
 sub analogWrite {
@@ -33,7 +42,18 @@ sub analogRead {
 
 sub pinMode {
 	my ($pin, $mode) = @_;
-	print STDERR "Mode $pin = $mode\n";
+	#print STDERR "Mode $pin = $mode\n";
+	{
+		open (my $out, ">", "/sys/class/gpio/export");
+		print $out $pin . "\n";
+		close $out;
+	}
+
+	{
+		open (my $out, ">", "/sys/class/gpio/gpio$pin/direction");
+		print $out $mode eq 'INPUT' ? 'in' : 'out';
+		close $out;
+	}
 	# XXX Register we want to use this pin
 	# XXX 
 }
